66306f4260ceecb3fe3884c19c02babfc9ab232a
Eko Satrio Handoyo Putro <94820695+Ekosattrio@users.noreply.github.com>
2025-11-25 13:49:07 +0700

    disuruh ivan push

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,24 +8,23 @@ use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
+        api: __DIR__ . '/../routes/api.php', 
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
 
+        // Redirect semua guest
         $middleware->redirectGuestsTo(function (Request $request) {
-
-            if ($request->is('penjual*', 'admin*')) {
-                return route('index');
-            }
-
             return route('index');
         });
 
+        // Bypass CSRF khusus Midtrans
         $middleware->validateCsrfTokens(except: [
             'midtrans/callback',
-            'midtrans/callback/*'
+            'midtrans/callback/*',
+            'api/midtrans/callback',
+            'api/midtrans/callback/*',
         ]);
 
     })
